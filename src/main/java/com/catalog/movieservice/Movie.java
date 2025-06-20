1f7987c49d3d7e69fad1a25549a18df6fd32109d
rheasharma22 <originalrhea@yahoo.in>
2025-06-20 14:57:34 -0500

    Add all updated files

--- src/main/java/com/catalog/movieservice/Movie.java
+++ src/main/java/com/catalog/movieservice/Movie.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GenerationType;
 @Entity
 public class Movie {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String genre;
